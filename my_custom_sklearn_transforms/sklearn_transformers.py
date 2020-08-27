12355aad61ffdc864649fe6390b4e31bbb06e0dd
AddyG <addy.gallareta@gmail.com>
2020-08-26 23:43:59 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import StandardScaler
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -16,16 +18,30 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class Standarized(BaseEstimator, TransformerMixin):
+class UnderSampling(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
 
-    def fit(self, X, y=None):
+    def fit(self, X, y):
         return self
     
-    def transform(self, X):
+    def transform(self, X,y):
         # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        sc = StandardScaler()
+        under = RandomUnderSampler(sampling_strategy=0.5)
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        return sc.fit_transform(data)
+    
+class OverSampling(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y):
+        return self
+    
+    def transform(self, X,y):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        over = RandomOverSampler(sampling_strategy=0.5)
         # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return sc.fit_transform(data)
