561f3a539aaba7c828520fe068cd8158eea1b27e
AddyG <addy.gallareta@gmail.com>
2020-09-06 23:15:19 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -15,4 +16,15 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
+class underSampling(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X,y):
+        under= RandomUnderSampler(sampling_strategy=0.2)
+        pipeline_input,pipeline_target=under.fit_sample(pipeline_input,pipeline_target)
+        return pipeline_input,pipeline_target
 
